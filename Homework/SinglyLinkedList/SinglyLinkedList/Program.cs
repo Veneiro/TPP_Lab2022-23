f199dd8cdedfc5ecb9f585a9240e9b882a42c9d1
Veneiro <50624734+Veneiro@users.noreply.github.com>
2023-03-30 17:22:22 +0200

    update

---
+++ Homework/SinglyLinkedList/SinglyLinkedList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SinglyLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
